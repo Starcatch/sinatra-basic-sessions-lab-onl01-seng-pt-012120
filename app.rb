64bec4bdf52e8459352f040a6e950d927f526215
Timur Mishiev <timurmishiev@gmail.com>
2020-05-07 21:16:39 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,6 +13,9 @@ class App < Sinatra::Base
   
   #displays the shopping cart contents 
   #sets the params in the session hash
-  
-  
+  post '/checkout' do
+    @sessions = session
+    item = params ["item"]
+    @sessions [:item] = item
+  end
 end
